b3a66899367567768f7fc872586ace3657d67ae4
igorekch1 <igor.chernyaev1999@gmail.com>
2019-03-06 04:51:34 +0200

    slider base functionality

---
+++ js/slider.js
@@ -0,0 +1,34 @@
+let slideIndex = 1;
+
+showSlides(slideIndex);
+
+function addSlide(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.querySelectorAll(".slider__block");
+    let dots = document.querySelectorAll(".slider-dot");    
+
+    if (n > slides.length) slideIndex = 1;
+
+    if (n < 1) slideIndex = slides.length;
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].classList.remove("current-dot");
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].classList.add("current-dot");
+}
+
+
